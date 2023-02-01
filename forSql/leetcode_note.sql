8d2c152463cf35b05456971b14740d2ca6b01227
tzuling.tseng <caitlinttl@gmail.com>
2023-02-01 20:57:29 +0800

    [update] update sql

---
+++ forSql/leetcode_note.sql
@@ -0,0 +1,45 @@
+-- 1071. Greatest Common Divisor of Strings Easy---------------------------------------
+
+Create table If Not Exists Person (personId int, firstName varchar(255), lastName varchar(255))
+Create table If Not Exists Address (addressId int, personId int, city varchar(255), state varchar(255))
+Truncate table Person
+insert into Person (personId, lastName, firstName) values ('1', 'Wang', 'Allen')
+insert into Person (personId, lastName, firstName) values ('2', 'Alice', 'Bob')
+Truncate table Address
+insert into Address (addressId, personId, city, state) values ('1', '2', 'New York City', 'New York')
+insert into Address (addressId, personId, city, state) values ('2', '3', 'Leetcode', 'California')
+
+Input: 
+Person table:
++----------+----------+-----------+
+| personId | lastName | firstName |
++----------+----------+-----------+
+| 1        | Wang     | Allen     |
+| 2        | Alice    | Bob       |
++----------+----------+-----------+
+Address table:
++-----------+----------+---------------+------------+
+| addressId | personId | city          | state      |
++-----------+----------+---------------+------------+
+| 1         | 2        | New York City | New York   |
+| 2         | 3        | Leetcode      | California |
++-----------+----------+---------------+------------+
+Output: 
++-----------+----------+---------------+----------+
+| firstName | lastName | city          | state    |
++-----------+----------+---------------+----------+
+| Allen     | Wang     | Null          | Null     |
+| Bob       | Alice    | New York City | New York |
++-----------+----------+---------------+----------+
+
+SELECT lastName , firstName ,city , state FROM Person LEFT JOIN Address ON Person.personId = Address.personId
+select firstName, lastName, city, state from person p left join address a on p.personId = a.personId;
+
+
+
+-- num_title Easy Medium---------------------------------------
+-- num_title Easy Medium---------------------------------------
+-- num_title Easy Medium---------------------------------------
+-- num_title Easy Medium---------------------------------------
+
+
