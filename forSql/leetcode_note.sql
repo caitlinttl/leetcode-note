8a270c718f9bddc7caba3b9629e1c9da1da9af53
tzuling.tseng <caitlinttl@gmail.com>
2023-02-04 16:29:01 +0800

    [update] update sql

--- forSql/leetcode_note.sql
+++ forSql/leetcode_note.sql
@@ -32,14 +32,173 @@ Output:
 | Bob       | Alice    | New York City | New York |
 +-----------+----------+---------------+----------+
 
+-- Answer:
 SELECT lastName , firstName ,city , state FROM Person LEFT JOIN Address ON Person.personId = Address.personId
 select firstName, lastName, city, state from person p left join address a on p.personId = a.personId;
 
 
 
+-- 181. Employees Earning More Than Their Managers Easy---------------------------------------
+Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int)
+Truncate table Employee
+insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3')
+insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4')
+insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', 'None')
+insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', 'None')
+
+Input: 
+Employee table:
++----+-------+--------+-----------+
+| id | name  | salary | managerId |
++----+-------+--------+-----------+
+| 1  | Joe   | 70000  | 3         |
+| 2  | Henry | 80000  | 4         |
+| 3  | Sam   | 60000  | Null      |
+| 4  | Max   | 90000  | Null      |
++----+-------+--------+-----------+
+Output: 
++----------+
+| Employee |
++----------+
+| Joe      |
++----------+
+Explanation: Joe is the only employee who earns more than his manager.
+
+-- Answer:
+select name as Employee from Employee e where managerId <> 'null' and salary > (select salary from Employee m where e.managerId = m.id); 
+select name as Employee from Employee e where salary > (select salary from Employee m where e.managerId = m.id); 
+select e.name Employee from Employee e JOIN Employee m on e.managerId = m.id where e.salary > m.salary;
+-- this solution faster than above
+
+
+
+
+-- 182. Duplicate Emails Easy---------------------------------------
+Create table If Not Exists Person (id int, email varchar(255))
+Truncate table Person
+insert into Person (id, email) values ('1', 'a@b.com')
+insert into Person (id, email) values ('2', 'c@d.com')
+insert into Person (id, email) values ('3', 'a@b.com')
+
+
+Input: 
+Person table:
++----+---------+
+| id | email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+Output: 
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+Explanation: a@b.com is repeated two times.
+
+-- Answer:
+select email Email from Person group by email having count(email) > 1;
+
+
+-- 183. Customers Who Never Order Easy---------------------------------------
+Create table If Not Exists Customers (id int, name varchar(255))
+Create table If Not Exists Orders (id int, customerId int)
+Truncate table Customers
+insert into Customers (id, name) values ('1', 'Joe')
+insert into Customers (id, name) values ('2', 'Henry')
+insert into Customers (id, name) values ('3', 'Sam')
+insert into Customers (id, name) values ('4', 'Max')
+Truncate table Orders
+insert into Orders (id, customerId) values ('1', '3')
+insert into Orders (id, customerId) values ('2', '1')
+
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+-- Answer:
+select c.name Customers from Customers c where c.id not in (select customerId from Orders);
+select c.name Customers from Customers c left join Orders o on c.id = o.customerId where o.customerId is null;
+
+-- 196. Delete Duplicate Emails Easy---------------------------------------
+Create table If Not Exists Person (Id int, Email varchar(255))
+Truncate table Person
+insert into Person (id, email) values ('1', 'john@example.com')
+insert into Person (id, email) values ('2', 'bob@example.com')
+insert into Person (id, email) values ('3', 'john@example.com')
+
+Input: 
+Person table:
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Output: 
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+Explanation: john@example.com is repeated two times. We keep the row with the smallest Id = 1.
+
+
+-- Answer:
+select * from Person where id in (select min(id) from Person group by email);
+delete p1 from person p1, person p2 where p1.email = p2.email and p1.id > p2.id;
+
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+with my_list as
+    (select email, min(id) as to_keep from Person
+    group by email)
+
+    delete from Person 
+    where id not in (select to_keep from my_list);
+
+
 -- num_title Easy Medium---------------------------------------
+-- Answer:
+
 -- num_title Easy Medium---------------------------------------
+-- Answer:
+
 -- num_title Easy Medium---------------------------------------
+-- Answer:
+
 -- num_title Easy Medium---------------------------------------
+-- Answer:
+
+-- num_title Easy Medium---------------------------------------
+-- Answer:
+
+-- num_title Easy Medium---------------------------------------
+-- Answer:
 
 
