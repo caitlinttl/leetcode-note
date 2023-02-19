97f5fc7d6198f659f8db522c71dc7d95d64348d4
tzuling.tseng <caitlinttl@gmail.com>
2023-02-19 15:38:00 +0800

    [update] update sql

--- forSql/leetcode_note.sql
+++ forSql/leetcode_note.sql
@@ -394,11 +394,27 @@ where (activity_date > "2019-06-27" AND activity_date <= "2019-07-27")
 group by activity_date;
 
 
--- num_title Easy Medium---------------------------------------
+-- 1148. Article Views I Easy---------------------------------------
 -- Answer:
+select distinct author_id as id from Views where author_id = viewer_id order by id asc;
 
--- num_title Easy Medium---------------------------------------
+-- 1179. Reformat Department Table Easy---------------------------------------
+-- openbook
 -- Answer:
+select id, 
+sum(IF(month = 'Jan', revenue, null)) as Jan_Revenue,
+sum(IF(month = 'Feb', revenue, null)) as Feb_Revenue,
+sum(IF(month = 'Mar', revenue, null)) as Mar_Revenue,
+sum(IF(month = 'Apr', revenue, null)) as Apr_Revenue,
+sum(IF(month = 'May', revenue, null)) as May_Revenue,
+sum(IF(month = 'Jun', revenue, null)) as Jun_Revenue,
+sum(IF(month = 'Jul', revenue, null)) as Jul_Revenue,
+sum(IF(month = 'Aug', revenue, null)) as Aug_Revenue,
+sum(IF(month = 'Sep', revenue, null)) as Sep_Revenue,
+sum(IF(month = 'Oct', revenue, null)) as Oct_Revenue,
+sum(IF(month = 'Nov', revenue, null)) as Nov_Revenue,
+sum(IF(month = 'Dec', revenue, null)) as Dec_Revenue
+from Department group by id
 
 
 
