860d183f30bda9e5801c0aa2aa8ac12a62b83113
tzuling.tseng <caitlinttl@gmail.com>
2023-03-28 08:42:43 +0800

    [update] update py

--- forPython/leetcode_note.py
+++ forPython/leetcode_note.py
@@ -509,6 +509,10 @@ class Solution:
             return True
         else: return False
 
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = "".join(c for c in s.lower() if c.isalnum())
+        return s == s[::-1]
 
 # 128. Longest Consecutive Sequence Medium---------------------------------------
 # openbook
